0c24d5fbbe5cca4c7c792725f1a5a56ae76f18fb
0xadk <0xadk@users.noreply.github.com>
2023-01-21 10:12:52 -0800

    feat: add highlight queries

---
+++ queries/highlights.scm
@@ -0,0 +1,39 @@
+[
+	"math"
+
+	"get"
+	"set"
+	"savepos"
+
+	"putvarchr"
+	"putarray"
+	"put"
+	"putdstring"
+	"putct"
+	"putbits"
+
+	"goto"
+
+	"if"
+	"elif"
+	"else"
+	"endif"
+
+	"for"
+	"next"
+
+	"callfunction"
+	"startfunction"
+	"endfunction"
+
+	"print"
+	"log"
+] @keyword
+
+(_ type: _ @type.builtin)
+(_ op: _ @operator)
+
+(call_function_statement name: _ @function)
+(start_function_statement name: _ @function)
+
+(comment) @comment
